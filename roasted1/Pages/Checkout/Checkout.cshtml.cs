e867f7159e074286e980c261466d2b21c00367ce
swarnimasingh <swarnimasingh156@gmail.com>
2023-02-12 19:41:14 +0000

    Added some CSS and changed some designed

---
+++ roasted1/Pages/Checkout/Checkout.cshtml.cs
@@ -0,0 +1,111 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using roasted1.Data;
+
+namespace roasted1.Pages.Checkout;
+
+[Authorize]
+
+public class Checkout : PageModel
+    
+{
+    public OrderHistory Order = new OrderHistory();
+    private readonly roasted1Context _db;
+    private readonly UserManager<ApplicationUser> _UserManager;
+    public IList<CheckoutItems> Items { get; private set; }
+    public decimal Total = 0;
+
+    public Checkout(roasted1Context db, 
+        UserManager<ApplicationUser> UserManager)
+    {
+        _db = db;
+        _UserManager = UserManager;
+    }
+
+    public async Task OnGetAsync()
+    {
+        
+        var user = await _UserManager.GetUserAsync(User);
+        CheckoutCustomer customer = await _db
+            .CheckoutCustomers
+            .FindAsync(user.Email);
+        
+    
+        
+        Items = _db.CheckoutItems.FromSqlRaw(
+            "SELECT tbl_menu.Id, tbl_menu.Price, " +
+            "tbl_menu.ItemName, " +
+            "BasketItems.BasketID, BasketItems.Quantity " +
+            "FROM tbl_menu INNER JOIN BasketItems " +
+            "ON tbl_menu.Id = BasketItems.StockID " +
+            "WHERE BasketID = {0}", customer.BasketID
+        ).ToList();
+
+    }
+    
+    
+    
+    public async Task<IActionResult> OnPostBuyAsync()
+    {
+        var currentOrder = _db.OrderHistories
+            .FromSqlRaw("SELECT OrderNo, Email From OrderHistories")
+            .OrderByDescending(b => b.OrderNo)
+            .FirstOrDefault();
+
+        if (currentOrder == null)
+        {
+            Order.OrderNo = 1;
+        }
+        else
+        {
+            Order.OrderNo = currentOrder.OrderNo + 1;
+        }
+
+        var user = await _UserManager.GetUserAsync(User);
+        Order.Email = user.Email;
+        _db.OrderHistories.Add(Order);
+
+        CheckoutCustomer customer = await _db
+            .CheckoutCustomers
+            .FindAsync(user.Email);
+
+        var basketItems = 
+            _db.BasketItems
+                .FromSqlRaw("SELECT StockID, BasketID, Quantity From BasketItems " +
+                            "WHERE BasketID = {0}", customer.BasketID)
+                .ToList();
+
+        foreach(var item in basketItems) 
+        {
+            OrderItem oi = new OrderItem
+            {
+                OrderNo = Order.OrderNo,
+                StockID = item.StockID,
+                Quantity = item.Quantity
+            };
+            _db.OrderItems.Add(oi);
+            _db.BasketItems.Remove(item);
+        }
+
+        await _db.SaveChangesAsync();
+
+        return RedirectToPage("/Checkout/ThankYou");
+    }
+
+    
+    
+
+}
+
+    
+ 
+     
+
+
+
+        
+        
+    
---
+++ roasted1/Pages/Checkout/ThankYou.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace roasted1.Pages.Checkout;
+
+public class ThankYou : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
